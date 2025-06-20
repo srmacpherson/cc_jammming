c484d296794803e84453056696b6dc4746cf1abf
srmacpherson <srmacpherson96@gmail.com>
2025-06-20 16:49:01 +0100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App () {
+    return (
+        <div>
+        <h1>
+          Ja<span className={styles.highlight}>mmm</span>ing
+        </h1>
+        <div className={styles.App}>
+          {/* <!-- Add a SearchBar component --> */}
+          
+          <div className={styles['App-playlist']}>
+            {/* <!-- Add a SearchResults component --> */}
+            {/* <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+        );
+}
+
+export default App;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+
+function SearchBar () {
+    return (
+        <div className={styles.SearchBar}>
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+        />
+        <button className={styles.SearchButton} >
+          SEARCH
+        </button>
+      </div>
+        );
+}
+
+export default SearchBar;
